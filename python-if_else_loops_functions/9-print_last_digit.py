147a343f2190c24c014c1b292ae750519b65b463
FoibeU <f.uwizeyima@alustudent.com>
2023-02-07 01:56:46 -0800

    task 9 lastD

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
-def print_last_digit(number):
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10)
+#!/usr/bin/python3
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
