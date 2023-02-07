edd16ac4f0c6e8acedfc49dabeb906baa27116ef
FoibeU <f.uwizeyima@alustudent.com>
2023-02-07 00:02:47 -0800

    sms of 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 islower = __import__('7-islower').islower
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
