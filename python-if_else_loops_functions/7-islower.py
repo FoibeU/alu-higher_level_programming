9a7fe85502f821fad5462077392e6d2db2e3e928
FoibeU <f.uwizeyima@alustudent.com>
2023-02-07 08:43:35 -0800

    slow check

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    """Function checks for lowercase characters."""
-        if ord(c) >= 97 and ord(c) <= 122:
-            return True
-        else:
-            return False
-
 islower = __import__('7-islower').islower
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
 print("3 is {}".format("lower" if islower("3") else "upper"))
 print("g is {}".format("lower" if islower("g") else "upper"))
-
-
