0b7373a365b5d6e7a7e46871ef5bbd214535364b
FoibeU <f.uwizeyima@alustudent.com>
2023-02-08 10:43:42 -0800

    uppercases

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for ch in str:
-        """Change to uppercase character."""
-        if ord(ch) >= 97 and ord(ch) <= 122:
-            ch = chr(ord(ch) - 32)
-            print("{}".format(ch), end="")
-            print()
+uppercase = __import__('8-uppercase').uppercase
+uppercase("best")
+uppercase("Best School 98 Battery street")
                                                             
