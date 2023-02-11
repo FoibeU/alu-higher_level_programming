812a536b3226dd4c46271f119a0fe4993981db22
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-02-11 20:16:29 +0000

    new format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 def uppercase(str):
-    for ch in str:
-        """Change to uppercase character."""
-        if ord(ch) >= 97 and ord(ch) <= 122:
-            ch = chr(ord(ch) - 32)
-        print("{}".format(ch), end="")
-    print()
-                                                            
+    new_str = ''
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            new_str += chr(ord(c) - 32)
+        else:
+            new_str += c
+    print('{}'.format(new_str))                                                            
