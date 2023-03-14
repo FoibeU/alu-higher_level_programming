601e3699c06abb02fedb489cda94571232aad099
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-03-14 10:50:33 +0000

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a square."""
+
+
+class Square:
+    """Representation of Square class"""
+
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
