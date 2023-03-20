79e61ca750499ba29392e030367e28b47b37e879
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-03-20 16:48:02 +0000

    task101

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-
-
-class LockedClass:
-    """LockedClass prevents dynamic creation of attributes other than
-    `first_name`
-    """
-    __slots__ = ['first_name']
-
-
-if __name__ == '__main__':
-    lc = LockedClass()
-    lc.first_name = "John"
-    try:
-        lc.last_name = "Snow"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+LockedClass = __import__('101-locked_class').LockedClass
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    lc.last_name = "Snow"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
