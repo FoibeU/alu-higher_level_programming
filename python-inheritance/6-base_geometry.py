726c04be6ca64fd9c4ddd43a2c6f7b4833ee0be9
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-03-22 15:35:05 +0000

    Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define an empty BaseGeometry class"""
+
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
