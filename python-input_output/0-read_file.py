4e80451851149be232fde6883b4c8f7c92fdc857
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-03-22 15:58:31 +0000

    make readable

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """Read file and print lines
-    Args:
-        filename (str): string of path to file
-    """
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
-if __name__ == '__main__':
-    read_file("my_file_0.txt")
+read_file = __import__('0-read_file').read_file
+
+read_file("my_file_0.txt")
