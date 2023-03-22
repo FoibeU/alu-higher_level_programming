569b2de52fa89875a233f4cc494e83850e3416f9
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-03-22 15:43:30 +0000

    some changes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""A subclass of class BaseGeometry"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method"""
+    def area(self):
+        """Area method not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator
+        Args:
+            name (str): name
+            value (int): integer value
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 1
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 1:
+            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class Rectangle that inherits from BaseGeometry
-    Subclass is initialized with args
-    Args:
-        width (int): width of the subclass Rectangle
-        height (int): heigth of the subclass Rectangle
-    """
+@@ -52,3 +34,5 @@ def __init__(self, width, height):
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = width
+    print(issubclass(Rectangle, BaseGeometry))
