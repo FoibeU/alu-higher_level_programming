513aab93539ebe2ffbc9ed1f732898c89e9a75e2
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-03-23 12:35:29 +0000

    read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-read_file = __import__('0-read_file').read_file
 
-read_file("my_file_0.txt")
+
+def read_file(filename=""):
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
