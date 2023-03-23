f1f0a4c6c0e6c11d656bbb57fa93d42277b3f082
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-03-23 12:47:12 +0000

    First Read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Reads a text file
+"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """Reads a text file (UTF8) and prints it to stdout.
+    Args:
+        filename (str): name of the file to read
+    """
+
+    with open(filename, encoding="utf-8") as file:
+        read_file = file.read()
+    print(read_file, end="")
