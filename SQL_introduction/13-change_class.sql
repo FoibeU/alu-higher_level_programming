021aeb3ae5b668c3287246fa9eea778ecc57fbd9
FoibeU <f.uwizeyima@alustudent.com>
2023-03-28 01:29:48 -0700

    change class

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Removes a record from a table
+-- Script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+DELETE FROM second_table WHERE score <= 5;
