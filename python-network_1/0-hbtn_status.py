aaf3b2723a4b5fa671d729dfd6bfd4a5bf184006
FoibeU <f.uwizeyima@gmail.com>
2023-04-12 14:06:44 -0700

    update hbtn status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
-"""
+
 
 if __name__ == '__main__':
-    from urllib import request
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as status:
-        status = status.read()
+    import urllib.request
 
-    url_type = type(status)
-    content = status
-    utf8_content = status.decode('UTF-8')
-    print("Body response:")
-    print(f"\t- type: {url_type}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {utf8_content}")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
