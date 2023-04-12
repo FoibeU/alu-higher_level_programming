559211d6f24fcf6d92e6fdb45bc1a25fccc05ce7
FoibeU <f.uwizeyima@gmail.com>
2023-04-12 13:24:43 -0700

    text indentation

--- python-test_driven_development/5-main.py
+++ python-test_driven_development/5-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 text_indentation = __import__('5-text_indentation').text_indentation
 
-text_indentation("""Lorem ipsum dolor  sit amet, consectetur adipiscing elit. \
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
 Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
 Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
 Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""Define text-indentation function."""
+"""
+Module composed by a function that prints 2 new lines after ".?:" characters
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
+    """ Function that prints 2 new lines after ".?:" characters
     Args:
-        text (string): The text to print.
+        text: input string
+    Returns:
+        No return
     Raises:
-        TypeError: If text is not a string.
+        TypeError: If text is not a string
     """
-    if not isinstance(text, str):
+
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    s = text[:]
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")
+
