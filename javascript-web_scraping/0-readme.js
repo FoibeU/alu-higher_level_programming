ec54e1e24c49246258c4d80f64e398a27167f77d
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-06-13 13:51:43 +0000

    Fix Standard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
+const file = process.argv.slice(2)[0];
 fs.readFile(file, 'utf-8', (err, contents) => {
   if (err) {
     console.log(err);
