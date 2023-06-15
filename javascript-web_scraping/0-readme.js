62d806c82db0101b180233bd09eda00a63ea32f5
FoibeU <f.uwizeyima@alustudent.com>
2023-06-15 15:09:53 +0200

    Read a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (error, contents) =>{
-    if (error){
-        console.log(error);
-    }else{
-        console.log(contents.toString());
-    }
-})
+
+fs.readFile(process.argv[2], (error, contents) => {
+  error ? console.log(error) : console.log(contents.toString());
+});
+
