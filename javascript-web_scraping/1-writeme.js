8315bd2f80ca5bf37c44cc2aa66318d38433f0ed
FoibeU <f.uwizeyima@alustudent.com>
2023-06-15 15:14:28 +0200

    write in file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8',(error) => {
-    if (error){
-        console.log(error);
-    }
+
+fs.writeFile(process.argv[2], process.argv[3], (error) => {
+  if (error) console.log(error);
 });
